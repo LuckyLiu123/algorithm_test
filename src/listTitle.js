d93da4d9e4e03a235399cfc34cfa7d4c891d8eaa
liukai <liukai@proitav.com>
2022-04-11 11:09:46 +0800

    添加链表算法题;

--- src/listTitle.js
+++ src/listTitle.js
@@ -30,15 +30,51 @@ const swapPairs2 = function(head){
     return dummyHead.next;
 }
 
-const head = createList([1, 2, 3, 4]);
-console.log('head:', head);
-console.log('swapPairs:', swapPairs2(head));
+// const head = createList([1, 2, 3, 4]);
+// console.log('head:', head);
+// console.log('swapPairs:', swapPairs2(head));
 
 
+//2. 合并两个有序链表(leetcode 21)
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+// 方法一：递归
+const mergeTwoLists1 = function(list1, list2){
+   if(list1 === null){
+       return list2;
+   }
+   if(list2 === null){
+       return list1;
+   }
+   if(list1.val < list2.val){
+       list1.next = mergeTwoLists(list1.next, list2);
+       return list1;
+   }else{
+       list2.next = mergeTwoLists(list1, list2.next);
+       return list2;
+   }
+}
 
+// 方法二: 迭代
+// 判断 l1 和 l2 哪一个链表的头节点的值更小，将较小值的节点添加到结果里，当一个节点被添加到结果里之后，将对应链表中的节点向后移一位。
+const mergeTwoLists2 = function(list1, list2){
+    let prehead = new ListNode(0);
+    let prev = prehead;
+    while(list1 !== null && list2 !== null){
+        if(list1.val < list2.val){
+            prev.next = list1;
+            list1 = list1.next;
+        }else{
+            prev.next = list2;
+            list2 = list2.next;
+        }
+        prev = prev.next;
+    }
+    prev.next = list1 === null ? list2 : list1;
+    return prehead.next;
+}
 
-
-
+const l1 = createList([1,2,4]), l2 = createList([1,3,4]);
+console.log('mergeTwoLists:', mergeTwoLists2(l1, l2));
 
 
 
