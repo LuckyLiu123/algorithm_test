b348e1b5a08982bcef60aae0ef17afa56c7a11c2
LuckyLiu123 <18229886430@163.com>
2022-04-12 11:49:59 +0800

    删除排序链表中的重复元素 II

--- src/listTitle.js
+++ src/listTitle.js
@@ -98,9 +98,25 @@ const deleteDuplicates = function(head){
 //4. 删除排序链表中的重复元素 II(leetcode 44)
 // 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
 const deleteDuplicates2 = function(head){
-
+    if(!head) return head;
+    let dummy = new ListNode(0, head);
+    let cur = dummy;
+    while(cur.next && cur.next.next){
+        if(cur.next.val === cur.next.next.val){
+            let x = cur.next.val;
+            while(cur.next && cur.next.val === x){
+                cur.next = cur.next.next;
+            }
+        }else{
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
 }
 
+// const head = createList([1,2,3,3,4,4,5]);
+const head = createList([1,1,1,2,3]);
+console.log('deleteDuplicates2:', deleteDuplicates2(head));
 
 
 
