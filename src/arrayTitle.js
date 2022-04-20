4eafed91365f6b159b284d52fc4722d8b0385ac4
LuckyLiu123 <18229886430@163.com>
2022-04-20 11:01:05 +0800

    合并区间(leetcode 56)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -134,15 +134,49 @@ function reverse(nums, start){
 
 // const nums = [1,2,3];
 // const nums = [3,2,1];
-const nums = [1,1,5];
-console.log('nextPermutation:', nextPermutation(nums));
+// const nums = [1,1,5];
+// console.log('nextPermutation:', nextPermutation(nums));
 
 
+//4. 合并区间(leetcode 56)
+// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，
+// 并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+/**
+ * 示例:
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+*/
+/**
+ * 思路：
+ * 如果我们按照区间的左端点排序，那么在排完序的列表中，可以合并的区间一定是连续的。
+ * 首先，我们将列表中的区间按照左端点升序排序。然后我们将第一个区间加入 merged 数组中，并按顺序依次考虑之后的每个区间：
+ * - 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，我们可以直接将这个区间加入数组 merged 的末尾；
+ * - 否则，它们重合，我们需要用当前区间的右端点更新数组 merged 中最后一个区间的右端点，将其置为二者的较大值。
+*/
+const merge = (intervals) => {
+    if(intervals.length === 0){
+        return [[]];
+    }
+    intervals.sort((a, b) => a[0] - b[0]);
 
+    const merged = [];
+    for(let i = 0; i < intervals.length; i++){
+        let L = intervals[i][0], R = intervals[i][1];
+        if(merged.length === 0 || merged[merged.length - 1][1] < L){
+            merged.push([L,R]);
+        }else{
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], R);
+        }
+    }
+    return merged;
+}
 
-
-
-
+const intervals = [[1,3],[2,6],[8,10],[15,18]];
+// const intervals = [[1,4],[4,5]];
+// const intervals = [[1,3]];
+// const intervals = [[1,4],[5,6]];
+console.log('merge:', merge(intervals));
 
 
 
