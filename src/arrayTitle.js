5ea9754c1d97804d43d4bd4376a05b27407c7f58
liukai <15818524733@139.com>
2022-04-30 21:37:28 +0800

    xxx

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -479,13 +479,39 @@ const majorityElement = (nums) => {
     }
 }
 // const nums = [3,2,3];
-const nums = [2,2,1,1,1,2,2];
-console.log('majorityElement:', majorityElement(nums));
+// const nums = [2,2,1,1,1,2,2];
+// console.log('majorityElement:', majorityElement(nums));
 
 
-
-
-
+//14. 买卖股票的最佳时机
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+*/
+/**
+ * 示例：
+ * 输入：[7,1,5,3,6,4]
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+*/
+const maxProfit = (prices) => {
+    let minPrice = Number.MAX_VALUE;
+    let maxPrice = 0;
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < minPrice){
+            minPrice = prices[i];
+        }else if(prices[i] - minPrice > maxPrice){
+            maxPrice = prices[i] - minPrice;
+        }
+    }
+    return maxPrice;
+}
+const prices = [7,1,5,3,6,4];
+// const prices = [7,6,4,3,1];
+// const prices = [2,4,1];
+console.log('maxProfit:', maxProfit(prices));
 
 
 
