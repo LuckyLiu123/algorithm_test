713be2947a37ae80197018974b1728bedc23db0b
liukai <15818524733@139.com>
2022-04-30 21:38:09 +0800

    买卖股票的最佳时机(leetcode 121)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -483,7 +483,7 @@ const majorityElement = (nums) => {
 // console.log('majorityElement:', majorityElement(nums));
 
 
-//14. 买卖股票的最佳时机
+//14. 买卖股票的最佳时机(leetcode 121)
 /**
  * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
  * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
