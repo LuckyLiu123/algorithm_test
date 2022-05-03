227ffcb339b6da3b2cff5b412d1474d70600edb2
liukai <15818524733@139.com>
2022-05-03 22:05:25 +0800

    买卖股票的最佳时机 II(leetcode 122)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -543,12 +543,40 @@ const getRow = (rowIndex) => {
     }
     return ret[rowIndex];
 }
-console.log('getRow:', getRow(0));
+// console.log('getRow:', getRow(0));
 
 
+//17. 买卖股票的最佳时机 II(leetcode 122)
+/**
+ * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+ * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+*/
+//方法一：贪心算法
+const maxProfit2 = (prices) => {
+    let ans = 0;
+    for(let i = 1; i < prices.length; i++) {
+        ans += Math.max(0, prices[i] - prices[i - 1]);
+    }
+    return ans;
+}
 
+//方法二：动态规划
+const maxProfit3 = (prices) => {
+    let dp0 = 0, dp1 = -prices[0];
+    for(let i = 1; i < prices.length; i++) {
+        let newDp0 = Math.max(dp0, dp1 + prices[i]);
+        let newDp1 = Math.max(dp1, dp0 - prices[i]);
+        dp0 = newDp0;
+        dp1 = newDp1;
+    }
+    return dp0;
+}
 
-
+const prices = [7,1,5,3,6,4];
+// const prices = [1,2,3,4,5];
+// const prices = [7,6,4,3,1];
+console.log('maxProfit2:', maxProfit3(prices));
 
 
 
