20506f84ea998ae9d956957d2d35ad92f49bdb43
LuckyLiu123 <18229886430@163.com>
2022-05-06 10:43:35 +0800

    被围绕的区域(leetcode 130)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -600,13 +600,59 @@ const subsets = (nums) => {
 }
 
 // const nums = [1, 2, 3];
-const nums = [0];
-console.log('subsets:', subsets(nums));
+// const nums = [0];
+// console.log('subsets:', subsets(nums));
 
 
+//19. 被围绕的区域(leetcode 130)
+// 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+/**
+ * 示例：
+ * 输入：board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+ * 输出：[["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+ * 解释：被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+*/
+//方法一：深度优先搜索
+const solve = (board) => {
+    const n = board.length;
+    if(n === 0) return;
+    const m = board[0].length;
 
+    const dfs = (board, x, y) => {
+        if(x < 0 || x >= n || y < 0 || y >= m || board[x][y] !== 'O'){
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x - 1, y);
+        dfs(board, x + 1, y);
+        dfs(board, x, y - 1);
+        dfs(board, x, y + 1);
+    }
 
+    for(let i = 0; i < n; i++){
+        dfs(board, i, 0);
+        dfs(board, i, m - 1);
+    }
+    for(let j = 1; j < m - 1; j++){
+        dfs(board, 0, j);
+        dfs(board, n - 1, j);
+    }
 
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            if(board[i][j] === 'A'){
+                board[i][j] = 'O';
+            }else if(board[i][j] === 'O'){
+                board[i][j] = 'X';
+            }
+        }
+    }
+    return board;
+}
+
+// const board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]];
+const board = [["X"]];
+console.log('solve:', solve(board));
 
 
 
