b0ce7ac108197831bb83b9e55f743598ca9ce017
LuckyLiu123 <18229886430@163.com>
2022-05-09 10:40:44 +0800

    最接近的三数之和(leetcode)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -726,14 +726,62 @@ const missingNumber2 = (nums) => {
     return n;
 }
 
-const nums = [3,0,1];
+// const nums = [3,0,1];
 // const nums = [0,1];
 // const nums = [9,6 ,4,2,3,5,7,0,1];
 // const nums = [0];
-console.log('missingNumber:', missingNumber2(nums));
+// console.log('missingNumber:', missingNumber2(nums));
 
 
+//23. 最接近的三数之和(leetcode)
+// 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+// 返回这三个数的和。假定每组输入只存在恰好一个解。
+const threeSumClosest = (nums, target) => {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    let best = 10000000;
+    for(let i = 0; i < n; i++){
+        // 保证和上一次枚举的元素不相等
+        if(i > 0 && nums[i] === nums[i - 1]){
+            continue;
+        }
+        // 使用双指针枚举 b 和 c
+        let j = i + 1, k = n - 1;
+        while(j < k){
+            let sum = nums[i] + nums[j] + nums[k];
+            // 如果和为 target 直接返回答案
+            if(sum === target){
+                return target;
+            }
+            // 根据差值的绝对值来更新答案
+            if(Math.abs(sum - target) < Math.abs(best - target)){
+                best = sum;
+            }
+            if(sum > target){
+                 // 如果和大于 target，移动 c 对应的指针
+                let k0 = k - 1;
+                // 移动到下一个不相等的元素
+                while(j < k0 && nums[k0] === nums[k]){
+                    --k0;
+                }
+                k = k0;
+            }else{
+                // 如果和小于 target，移动 b 对应的指针
+                let j0 = j + 1;
+                // 移动到下一个不相等的元素
+                while( j0 < k && nums[j0] === nums[j]){
+                    ++j0;
+                }
+                j = j0;
+            }
+        }
+    }
+    return best;
+}
 
+// const nums = [-1,2,1,-4], target = 1;
+const nums = [0,0,0], target = 1;
+console.log('threeSumClosest:', threeSumClosest(nums, target));
 
 
 
