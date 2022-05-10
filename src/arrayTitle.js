52614e5a6a44a046e9fa88c0b8fa4f8dd7165800
LuckyLiu123 <18229886430@163.com>
2022-05-10 11:07:10 +0800

    两数之和(leetcode 1)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -732,6 +732,25 @@ const missingNumber2 = (nums) => {
 // const nums = [0];
 // console.log('missingNumber:', missingNumber2(nums));
 
+//23. 两数之和(leetcode 1)
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。你可以按任意顺序返回答案。
+const twoSum = (nums, target) => {
+    let map = new Map();
+    const n = nums.length;
+    for(let i = 0; i < n; i++){
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+// const nums = [2,7,11,15], target = 9;
+// const nums = [3,2,4], target = 6;
+const nums = [3,3], target = 6;
+console.log('twoSum:', twoSum(nums, target));
+
 
 //23. 三数之和(leetcode 15)
 // 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
@@ -771,8 +790,8 @@ const threeSum = (nums) => {
     return ans;
 }
 // const nums = [-1,0,1,2,-1,-4];
-const nums = [0];
-console.log('threeSum:', threeSum(nums));
+// const nums = [0];
+// console.log('threeSum:', threeSum(nums));
 
 
 
