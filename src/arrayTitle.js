36b46aa000ae9716a184ca0a3c6892cac56b2365
liukai <15818524733@139.com>
2022-05-15 21:33:41 +0800

    汇总区间(leetcode 228)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1011,17 +1011,44 @@ const containsNearbyDuplicate = (nums) => {
     }
     return false;
 }
-const nums = [1,2,3,1], k = 3;
+// const nums = [1,2,3,1], k = 3;
 // const nums = [1,0,1,1], k = 1;
 // const nums = [1,2,3,1,2,3], k = 2;
-console.log('containsNearbyDuplicate:', containsNearbyDuplicate(nums, k));
+// console.log('containsNearbyDuplicate:', containsNearbyDuplicate(nums, k));
 
 
+// 31. 汇总区间(leetcode 228)
+/**
+ * 给定一个  无重复元素 的 有序 整数数组 nums 。返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表 。
+ * 也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个范围但不属于 nums 的数字 x 。
+ * 列表中的每个区间范围 [a,b] 应该按如下格式输出：
+ * · "a->b" ，如果 a != b
+ * · "a" ，如果 a == b
+*/
+const summaryRanges = (nums) => {
+    const ans = [];
+    const n = nums.length;
+    let i = 0;
+    while(i < n){
+        const low = i;
+        i++;
+        while(i < n && nums[i] === nums[i - 1] + 1){
+            i++;
+        }
+        const high = i - 1;
+        const temp = ['' + nums[low]];
+        if(low < high){
+            temp.push('->');
+            temp.push(nums[high]);
+        }
+        ans.push(temp.join(''));
+    }
+    return ans;
+}
 
-
-
-
-
+// const nums = [0,1,2,4,5,7];
+const nums = [0,2,3,4,6,8,9];
+console.log('summaryRanges:', summaryRanges(nums));
 
 
 
