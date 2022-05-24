85e28b0f0389c0bc4217e361d1569b30dcc36332
LuckyLiu123 <18229886430@163.com>
2022-05-24 10:21:16 +0800

    两数之和 II - 输入有序数组(leetcode 167)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1152,14 +1152,37 @@ const maxProduct = (nums) => {
 // const nums = [2,3,-2,4];
 // const nums = [-2,0,-1];
 // const nums = [-2];
-const nums = [-2, 1, -1, 1];
-console.log('maxProduct:', maxProduct(nums));
+// const nums = [-2, 1, -1, 1];
+// console.log('maxProduct:', maxProduct(nums));
 
 
+// 36. 两数之和 II - 输入有序数组(leetcode 167)
+/**
+ * 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，
+ * 则 1 <= index1 < index2 <= numbers.length 。
+ * 以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+ * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+ * 你所设计的解决方案必须只使用常量级的额外空间。
+*/
+const twoSum1 = (numbers, target) => {
+    let low = 0, high = numbers.length - 1;
+    while(low < high){
+        const sum = numbers[low] + numbers[high];
+        if(sum === target){
+            return [low + 1, high + 1];
+        }else if(sum < target){
+            low++;
+        }else{
+            high--;
+        }
+    }
+    return [-1, -1];
+}
 
-
-
-
+// const numbers = [2,7,11,15], target = 9;
+// const numbers = [2,3,4], target = 6;
+const numbers = [-1,0], target = -1;
+console.log('twoSum1:', twoSum1(numbers, target));
 
 
 
