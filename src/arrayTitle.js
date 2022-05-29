3da37d06eb8623ac8d8d727a7bb3e8df614f6ce7
liukai <15818524733@139.com>
2022-05-29 22:02:27 +0800

    分发饼干(leetcode 455)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1411,12 +1411,35 @@ const minMoves = (nums) => {
 }
 
 // const nums = [1,2,3];
-const nums = [1,1,1];
-console.log('minMoves:', minMoves(nums));
+// const nums = [1,1,1];
+// console.log('minMoves:', minMoves(nums));
 
 
-
-
+// 44. 分发饼干(leetcode 455)
+/**
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
+ * 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+*/
+const findContentChildren = (g, s) => {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    const gLen = g.length, sLen = s.length;
+    let count = 0;
+    for(let i = 0, j = 0; i < gLen && j < sLen; i++, j++) {
+        while(j < sLen && g[i] > s[j]){
+            j++;
+        }
+        if(j < sLen){
+            count++;
+        }
+    }
+    return count;
+}
+// const g = [1,2,3], s = [1,1];
+// const g = [1,2], s = [1,2,3];
+const g = [10,9,8,7], s = [5,6,7,8];
+console.log('findContentChildren:', findContentChildren(g, s));
 
 
 
