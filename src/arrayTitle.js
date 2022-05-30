9b2b24ed5681612e8296bec708a72a50d48da602
LuckyLiu123 <18229886430@163.com>
2022-05-30 10:43:27 +0800

    打家劫舍(leetcode 198)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1438,12 +1438,34 @@ const findContentChildren = (g, s) => {
 }
 // const g = [1,2,3], s = [1,1];
 // const g = [1,2], s = [1,2,3];
-const g = [10,9,8,7], s = [5,6,7,8];
-console.log('findContentChildren:', findContentChildren(g, s));
+// const g = [10,9,8,7], s = [5,6,7,8];
+// console.log('findContentChildren:', findContentChildren(g, s));
 
 
+// 45. 打家劫舍(leetcode 198)
+// 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+// 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+const rob = (nums) => {
+    if(nums == null || nums.length === 0){
+        return 0;
+    }
+    if(nums.length === 1){
+        return nums[0];
+    }
+    const n = nums.length;
+    let first = nums[0], second = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < n; i++){
+        const temp = second;
+        second = Math.max(first + nums[i], second);
+        first = temp;
+    }
+    return second;
+}
 
-
+// const nums = [1,2,3,1];
+const nums = [2,7,9,3,1];
+console.log('rob:', rob(nums));
 
 
 
