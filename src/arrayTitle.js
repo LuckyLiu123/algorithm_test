092ddaba4ad9af34f2c2f8c575d272a088cef3b1
LuckyLiu123 <18229886430@163.com>
2022-06-01 11:09:07 +0800

    岛屿的周长(leetcode 463)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1593,14 +1593,43 @@ const countPrimes2 = (n) => {
     return ans;
 }
 
-const n = 367184;
-console.log('countPrimes:', countPrimes2(n));
+// const n = 367184;
+// console.log('countPrimes:', countPrimes2(n));
 
 
+// 49. 岛屿的周长(leetcode 463)
+/**
+ * 给定一个 row x col 的二维网格地图 grid ，其中：grid[i][j] = 1 表示陆地， grid[i][j] = 0 表示水域。
+ * 网格中的格子 水平和垂直 方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+*/
+const islandPerimeter = (grid) => {
+    const dx = [0, 1, 0, -1];
+    const dy = [1, 0, -1, 0];
+    const n = grid.length, m = grid[0].length;
+    let ans = 0;
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            if(grid[i][j]){
+                let cnt = 0;
+                for(let k = 0; k < 4; ++k){
+                    let tx = i + dx[k];
+                    let ty = j + dy[k];
+                    if(tx < 0 || tx >= n || ty < 0 || ty >= m || !grid[tx][ty]){
+                        cnt += 1;
+                    }
+                }
+                ans += cnt;
+            }
+        }
+    }
+    return ans;
+}
 
-
-
-
+// const grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]];
+// const grid = [[1]];
+const grid = [[1,0]];
+console.log('islandPerimeter:', islandPerimeter(grid));
 
 
 
