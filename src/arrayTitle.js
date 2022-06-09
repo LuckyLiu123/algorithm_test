f29100cdc47760a944b0503119e8ab9d1fd0fb04
LuckyLiu123 <18229886430@163.com>
2022-06-09 11:03:22 +0800

    存在重复元素 III(leetcode 220)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -1885,14 +1885,58 @@ const combinationSum32 = (k, n) => {
     return res;
 }
 
-const k = 3, n = 7;
+// const k = 3, n = 7;
 // const k = 3, n = 9;
-console.log('combinationSum3:', combinationSum32(k, n));
+// console.log('combinationSum3:', combinationSum32(k, n));
 
 
+// 56. 存在重复元素 III(leetcode 220)
+// 给你一个整数数组 nums 和两个整数 k 和 t 。请你判断是否存在 两个不同下标 i 和 j，使得 abs(nums[i] - nums[j]) <= t ，同时又满足 abs(i - j) <= k 。
+// 如果存在则返回 true，不存在返回 false。
+const containsNearbyAlmostDuplicate1 = (nums, k, t) => {
+    const n = nums.length;
+    for(let i = 0; i < n; i++){
+        for(let j = i; j < n; j++){
+            if(Math.abs(nums[i] - nums[j]) <= t && Math.abs(i - j) <= k && i !== j){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
+// 方法二：桶
+const containsNearbyAlmostDuplicate2 = (nums, k, t) => {
+    const n = nums.length;
+    const mp = new Map();
+    for(let i = 0; i < n; i++){
+        const x = nums[i];
+        const id = getID(x, t + 1);
+        if(mp.has(id)){
+            return true;
+        }
+        if(mp.has(id - 1) && Math.abs(x - mp.get(id - 1)) <= t){
+            return true;
+        }
+        if(mp.has(id + 1) && Math.abs(x - mp.get(id + 1)) <= t){
+            return true;
+        }
+        mp.set(id, x);
+        if(i >= k){
+            mp.delete(getID(nums[i - k], t + 1));
+        }
+    }
+    return false;
+}
 
+const getID = (x, w) => {
+    return x < 0 ? Math.floor((x + 1) / w) - 1 : Math.floor(x / w);
+}
 
+// const nums = [1,2,3,1], k = 3, t = 0;
+// const nums = [1,0,1,1], k = 1, t = 2;
+const nums = [1,5,9,1,5,9], k = 2, t = 3;
+console.log('containsNearbyAlmostDuplicate:', containsNearbyAlmostDuplicate2(nums, k, t));
 
 
 
