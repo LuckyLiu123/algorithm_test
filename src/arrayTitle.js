9c817be7f8f96158a4ae2e9dc429cf2f3c5cb0e7
liukai <15818524733@139.com>
2022-06-12 10:36:07 +0800

    最大正方形(leetcode 221)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2069,12 +2069,37 @@ const productExceptSelf2 = (nums) => {
     return answer;
 }
 
-const nums = [1,2,3,4];
+// const nums = [1,2,3,4];
 // const nums = [-1,1,0,-3,3];
-console.log('productExceptSelf:', productExceptSelf2(nums));
+// console.log('productExceptSelf:', productExceptSelf2(nums));
 
 
-
+// 60. 最大正方形(leetcode 221)
+// 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+// 方法一：动态规划
+// 状态转移方程：dp(i,j) = min(dp(i−1, j), dp(i−1, j−1), dp(i, j−1)) + 1
+const maximalSquare = (matrix) => {
+    let maxSide = 0;
+    const row = matrix.length, column = matrix[0].length;
+    const dp = new Array(row).fill(0).map(() => new Array(column).fill(0));
+    for(let i = 0; i < row; i++){
+        for(let j = 0; j < column; j++){
+            if(matrix[i][j] === '1'){
+                if(i === 0 || j === 0){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1;
+                }
+            }
+            maxSide = Math.max(maxSide, dp[i][j]);
+        }
+    }
+    return maxSide * maxSide;
+}
+// const matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]];
+// const matrix = [["0","1"],["1","0"]];
+const matrix = [["0"]];
+console.log('maximalSquare:', maximalSquare(matrix));
 
 
 
