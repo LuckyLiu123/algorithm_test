d2e89cc65cdf3d0bd53eaac460f62b0c1aad2473
LuckyLiu123 <18229886430@163.com>
2022-06-14 11:14:22 +0800

    接雨水(leetcode 42)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2130,6 +2130,509 @@ const maxSlidingWindow = (nums, k) => {
     return ans;
 }
 
-const nums = [1,3,-1,-3,5,3,6,7], k = 3;
+// const nums = [1,3,-1,-3,5,3,6,7], k = 3;
 // const nums = [1], k = 1;
-console.log('maxSlidingWindow:', maxSlidingWindow(nums, k));
+// console.log('maxSlidingWindow:', maxSlidingWindow(nums, k));
+
+
+// 62. 接雨水(leetcode 42)
+// 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+// 方法一：动态规划
+/**
+ * 思路：
+ * 创建两个长度为 nn 的数组 leftMax 和 rightMax。对于 0≤i<n，leftMax[i] 表示下标 i 及其左边的位置中，height 的最大高度，
+ * rightMax[i] 表示下标 i 及其右边的位置中，height 的最大高度。
+ * 显然，leftMax[0]=height[0]，rightMax[n−1]=height[n−1]。两个数组的其余元素的计算如下：
+ * - 当 1≤i≤n−1 时，leftMax[i] = max(leftMax[i−1], height[i])；
+ * - 当 0≤i≤n−2 时，rightMax[i] = max(rightMax[i+1], height[i])。
+ * 因此可以正向遍历数组 height 得到数组 leftMax 的每个元素值，反向遍历数组 height 得到数组 rightMax 的每个元素值。
+ * 在得到数组 leftMax 和 rightMax 的每个元素值之后，对于 0≤i<n，下标 i 处能接的雨水量等于 min(leftMax[i], rightMax[i]) − height[i]。
+ * 遍历每个下标位置即可得到能接的雨水总量。
+*/
+const trap1 = (height) => {
+    const n = height.length;
+    if(n === 0){
+        return 0;
+    }
+
+    const leftMax = new Array(n).fill(0);
+    leftMax[0] = height[0];
+    for(let i = 1; i < n; i++){
+        leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+    }
+
+    const rightMax = new Array(n).fill(0);
+    rightMax[n - 1] = height[n - 1];
+    for(let i = n - 2; i >= 0; i--){
+        rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+    }
+
+    let ans = 0;
+    for(let i = 0; i < n; i++){
+        ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+    }
+    return ans;
+}
+
+// 方法二：单调栈
+/**
+ * 思路：
+ * 维护一个单调栈，单调栈存储的是下标，满足从栈底到栈顶的下标对应的数组 height 中的元素递减。
+ * 从左到右遍历数组，遍历到下标 i 时，如果栈内至少有两个元素，记栈顶元素为 top，top 的下面一个元素是 left，
+ * 则一定有 height[left] ≥ height[top]。如果 height[i] > height[top]，则得到一个可以接雨水的区域，
+ * 该区域的宽度是 i−left−1，高度是 min(height[left], height[i]) − height[top]，根据宽度和高度即可计算得到该区域能接的雨水量。
+*/
+const trap2 = (height) => {
+    let ans = 0;
+    const stack = [];
+    const n = height.length;
+    for(let i = 0; i < n; i++){
+        while(stack.length && height[i] > height[stack[stack.length - 1]]){
+            const top = stack.pop();
+            if(!stack.length){
+                break;
+            }
+            const left = stack[stack.length - 1];
+            const curWidth = i - left - 1;
+            const curHeight = Math.min(height[left], height[i]) - height[top];
+            ans += curWidth * curHeight;
+        }
+        stack.push(i);
+    }
+    return ans;
+}
+
+// 方法三：双指针
+/**
+ * 思路：
+ * 维护两个指针 left 和 right，以及两个变量 leftMax 和 rightMax，初始时 left = 0, right = n − 1, leftMax = 0, rightMax = 0。
+ * 指针 left 只会向右移动，指针 right 只会向左移动，在移动指针的过程中维护两个变量 leftMax 和 rightMax 的值。
+ * 当两个指针没有相遇时，进行如下操作：
+ * - 使用 height[left] 和 height[right] 的值更新 leftMax 和 rightMax 的值；
+ * - 如果 height[left] < height[right]，则必有 leftMax < rightMax，下标 left 处能接的雨水量等于 leftMax − height[left]，
+ * 将下标 left 处能接的雨水量加到能接的雨水总量，然后将 left 加 1（即向右移动一位）；
+ * - 如果 height[left] ≥ height[right]，则必有 leftMax ≥ rightMax，下标 right 处能接的雨水量等于 rightMax − height[right]，
+ * 将下标 right 处能接的雨水量加到能接的雨水总量，然后将 right 减 1（即向左移动一位）。
+ * 当两个指针相遇时，即可得到能接的雨水总量。
+*/
+const trap3 = (height) => {
+    let ans = 0;
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    while(left < right){
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+        if(height[left] < height[right]){
+            ans += leftMax - height[left];
+            left++;
+        }else{
+            ans += rightMax - height[right];
+            right--;
+        }
+    }
+    return ans;
+}
+
+
+// const height = [0,1,0,2,1,0,1,3,2,1,2,1];
+const height = [4,2,0,3,2,5];
+console.log('trap:', trap3(height));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
