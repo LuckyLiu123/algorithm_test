1f332e8eede1b8966981acb1112cf8ce3bdcc645
LuckyLiu123 <18229886430@163.com>
2022-06-16 10:55:50 +0800

    搜索二维矩阵 II(leetcode 240)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2279,14 +2279,72 @@ const candy = (ratings) => {
 
 // const ratings = [1,0,2];
 // const ratings = [1,2,2];
-const ratings = [1,2,3];
-console.log('candy:', candy(ratings));
+// const ratings = [1,2,3];
+// console.log('candy:', candy(ratings));
 
 
+// 64. 搜索二维矩阵 II(leetcode 240)
+/**
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * - 每行的元素从左到右升序排列。
+ * - 每列的元素从上到下升序排列。
+*/
+// 方法一：Z 字形查找
+/**
+ * 我们可以从矩阵 matrix 的右上角 (0,n−1) 进行搜索。在每一步的搜索过程中，如果我们位于位置 (x,y)，那么我们希望在以 matrix 的左下角
+ * 为左下角、以 (x,y) 为右上角的矩阵中进行搜索，即行的范围为 [x,m−1]，列的范围为 [0,y]：
+ * - 如果 matrix[x,y] = target，说明搜索完成；
+ * - 如果 matrix[x,y] > target，由于每一列的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 y 列的元素都是严格大于 target 的，
+ * 因此我们可以将它们全部忽略，即将 y 减少 1；
+ * - 如果 matrix[x,y] < target，由于每一行的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第 x 行的元素都是严格小于 target 的，
+ * 因此我们可以将它们全部忽略，即将 x 增加 1。
+ * 在搜索的过程中，如果我们超出了矩阵的边界，那么说明矩阵中不存在 \textit{target}target。
+*/
+const searchMatrix1 = (matrix, target) => {
+    const m = matrix.length, n = matrix[0].length;
+    let x = 0, y = n - 1;
+    while(x < m && y >= 0){
+        if(matrix[x][y] === target){
+            return true;
+        }
+        if(matrix[x][y] > target){
+            y--;
+        }else{
+            x++;
+        }
+    }
+    return false;
+}
 
+// 方法二：二分查找
+const searchMatrix2 = (matrix, target) => {
+    for(const row of matrix){
+        const index = search(row, target);
+        if(index >= 0){
+            return true;
+        }
+    }
+    return false;
+}
 
+const search = (nums, target) => {
+    let left = 0, right = nums.length - 1;
+    while(left <= right){
+        const mid = Math.floor((right - left) / 2) + left;
+        if(nums[mid] === target){
+            return mid;
+        }else if(nums[mid] > target){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
 
-
+const matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5;
+// const matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20;
+console.log('searchMatrix:', searchMatrix2(matrix, target));
 
 
 
