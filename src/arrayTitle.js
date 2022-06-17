613d058ee4d87059b7ef9a66f32a8d3689fde943
LuckyLiu123 <18229886430@163.com>
2022-06-17 10:45:40 +0800

    搜索旋转排序数组 II(leetcode 81)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2342,16 +2342,59 @@ const search = (nums, target) => {
     return -1;
 }
 
-const matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5;
+// const matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5;
 // const matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20;
-console.log('searchMatrix:', searchMatrix2(matrix, target));
+// console.log('searchMatrix:', searchMatrix2(matrix, target));
 
 
+// 65. 搜索旋转排序数组 II(leetcode 81)
+/**
+ * 已知存在一个按非降序排列的整数数组 nums ，数组中的值不必互不相同。
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转 ，
+ * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+ * 例如， [0,1,2,4,4,4,5,6,6,7] 在下标 5 处经旋转后可能变为 [4,5,6,6,7,0,1,2,4,4] 。
+ * 给你 旋转后 的数组 nums 和一个整数 target ，请你编写一个函数来判断给定的目标值是否存在于数组中。
+ * 如果 nums 中存在这个目标值 target ，则返回 true ，否则返回 false 。
+ * 你必须尽可能减少整个操作步骤。
+*/
+// 方法一：二分查找
+const search3 = (nums, target) => {
+    const n = nums.length;
+    if(n === 0){
+        return false;
+    }
+    if(n === 1){
+        return nums[0] === target;
+    }
+    let l = 0, r = n - 1;
+    while(l <= r){
+        const mid = Math.floor((r - l) / 2) + l;
+        if(nums[mid] === target){
+            return true;
+        }
+        if(nums[l] === nums[mid] && nums[mid] === nums[r]){
+            l++;
+            r--;
+        }else if(nums[l] <= nums[mid]){
+            if(nums[l] <= target && target < nums[mid]){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }else{
+            if(nums[mid] < target && target <= nums[n - 1]){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+    }
+    return false;
+}
 
-
-
-
-
+// const nums = [2,5,6,0,0,1,2], target = 0;
+const nums = [2,5,6,0,0,1,2], target = 3;
+console.log('search3:', search3(nums, target));
 
 
 
