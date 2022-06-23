fb14dd2c302bf68d005e262a3064f803091d2026
LuckyLiu123 <18229886430@163.com>
2022-06-23 11:12:37 +0800

    平衡二叉树(leetcode 110)

--- src/binaryTreeTitle.js
+++ src/binaryTreeTitle.js
@@ -137,8 +137,44 @@ const isSameTree = (p, q) => {
 }
 
 
+// 6. 平衡二叉树(leetcode 110)
+// 给定一个二叉树，判断它是否是高度平衡的二叉树。
+// 本题中，一棵高度平衡二叉树定义为：
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+// 方法一：自顶向下的递归
+const isBalanced1 = (root) => {
+    if(root == null){
+        return true;
+    }else{
+        return Math.abs(height1(root.left) - height1(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+}
 
+const height1 = (root) => {
+    if(root == null){
+        return 0;
+    }else{
+        return Math.max(height1(root.left), height1(root.right)) + 1;
+    }
+}
 
+// 方法二：自底向上的递归
+const isBalanced2 = (root) => {
+    return height2(root) >= 0;
+}
+
+const height2 = (root) => {
+    if(root == null){
+        return 0;
+    }
+    let leftHeight = height2(root.left);
+    let rightHeight = height2(root.right);
+    if(leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1){
+        return -1;
+    }else{
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
 
 
 
