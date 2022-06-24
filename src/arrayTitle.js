d1207c086ff58170e19e918b9f991d49115da464
LuckyLiu123 <18229886430@163.com>
2022-06-24 10:47:44 +0800

    旋转图像(leetcode 48)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2484,13 +2484,33 @@ const permuteUnique = (nums) => {
     return res;
 }
 
-const nums = [1,1,2];
-console.log('permuteUnique:', permuteUnique(nums));
+// const nums = [1,1,2];
+// console.log('permuteUnique:', permuteUnique(nums));
 
 
+// 69. 旋转图像(leetcode 48)
+// 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+// 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+const rotate = (matrix) => {
+    const n = matrix.length;
+    // 水平翻转
+    for(let i = 0; i < Math.floor(n / 2); i++){
+        for(let j = 0; j < n; j++){
+            [matrix[i][j], matrix[n - i - 1][j]] = [matrix[n - i - 1][j], matrix[i][j]];
+        }
+    }
+    // 主对角线翻转
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < i; j++){
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    return matrix;
+}
 
-
-
+const matrix = [[1,2,3],[4,5,6],[7,8,9]];
+// const matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+console.log('rotate:', rotate(matrix));
 
 
 
