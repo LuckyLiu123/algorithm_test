aefc225931a7cd77764d82c448dd7c9e6554dddf
LuckyLiu123 <18229886430@163.com>
2022-06-27 10:44:46 +0800

    螺旋矩阵(leetcode 54)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2593,14 +2593,68 @@ const groupAnagrams = (strs) => {
     return Array.from(map.values());
 }
 
-const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
-console.log('groupAnagrams:', groupAnagrams(strs));
+// const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+// console.log('groupAnagrams:', groupAnagrams(strs));
 
 
+// 73. 螺旋矩阵(leetcode 54)
+// 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+// 方法一：模拟
+const spiralOrder1 = (matrix) => {
+    if(!matrix.length || !matrix[0].length){
+        return [];
+    }
+    const rows = matrix.length, columns = matrix[0].length;
+    const visited = new Array(rows).fill(0).map(() => new Array(columns).fill(false));
+    const total = rows * columns;
+    const order = new Array(total).fill(0);
 
+    let directionIndex = 0, row = 0, column = 0;
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    for(let i = 0; i < total; i++){
+        order[i] = matrix[row][column];
+        visited[row][column] = true;
+        const nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+        if(!(nextRow >= 0 && nextRow < rows && nextColumn >= 0 && nextColumn < columns && !visited[nextRow][nextColumn])){
+            directionIndex = (directionIndex + 1) % 4;
+        }
+        row += directions[directionIndex][0];
+        column += directions[directionIndex][1];
+    }
+    return order;
+}
 
+// 方法二：按层模拟
+const spiralOrder2 = (matrix) => {
+    if(!matrix.length || !matrix[0].length){
+        return [];
+    }
+    const rows = matrix.length, columns = matrix[0].length;
+    const order = [];
+    let left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+    while(left <= right && top <= bottom){
+        for(let column = left; column <= right; column++){
+            order.push(matrix[top][column]);
+        }
+        for(let row = top + 1; row <= bottom; row++){
+            order.push(matrix[row][right]);
+        }
+        if(left < right && top < bottom){
+            for(let column = right - 1; column > left; column--){
+                order.push(matrix[bottom][column]);
+            }
+            for(let row = bottom; row > top; row--){
+                order.push(matrix[row][left]);
+            }
+        }
+        [left, right, top, bottom] = [left + 1, right - 1, top + 1, bottom - 1];
+    }
+    return order;
+}
 
-
+const matrix = [[1,2,3],[4,5,6],[7,8,9]];
+// const matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+console.log('spiralOrder:', spiralOrder2(matrix));
 
 
 
