b0570ecbac69da30f47a935e5f7fddf6a4beaaa3
LuckyLiu123 <18229886430@163.com>
2022-07-01 10:40:02 +0800

    为运算表达式设计优先级(leetcode 241)

--- src/stringTitle.js
+++ src/stringTitle.js
@@ -215,14 +215,81 @@ const decode = (shortUrl) => {
     return dataBase.get(id);
 }
 
-const url = "https://leetcode.com/problems/design-tinyurl";
-console.log('decode:', decode(encode(url)));
+// const url = "https://leetcode.com/problems/design-tinyurl";
+// console.log('decode:', decode(encode(url)));
 
 
+// 8. 为运算表达式设计优先级(leetcode 241)
+// 给你一个由数字和运算符组成的字符串 expression ，按不同优先级组合数字和运算符，计算并返回所有可能组合的结果。你可以 按任意顺序 返回答案。
+// 生成的测试用例满足其对应输出值符合 32 位整数范围，不同结果的数量不超过 10^4 。
+const ADDITION = -1;
+const SUBTRACTION = -2;
+const MULTIPLICATION = -3;
 
+const diffWaysToCompute = (expression) => {
+    const ops = [];
+    for(let i = 0; i < expression.length;){
+        if(!isDigit(expression[i])){
+            if(expression[i] === '+'){
+                ops.push(ADDITION);
+            }else if(expression[i] === '-'){
+                ops.push(SUBTRACTION);
+            }else{
+                ops.push(MULTIPLICATION);
+            }
+            i++;
+        }else{
+            let t = 0;
+            while(i < expression.length && isDigit(expression[i])){
+                t = t * 10 + expression[i].charCodeAt() - '0'.charCodeAt();
+                i++;
+            }
+            ops.push(t);
+        }
+    }
 
+    const dp = new Array(ops.length).fill(0).map(() => new Array(ops.length).fill(0));
+    for(let i = 0; i < ops.length; i++){
+        for(let j = 0; j < ops.length; j++){
+            dp[i][j] = [];
+        }
+    }
 
+    return diffDfs(dp, 0, ops.length - 1, ops);
+}
 
+const diffDfs = (dp, l, r, ops) => {
+    if(dp[l][r].length === 0){
+        if(l === r){
+            dp[l][r].push(ops[l]);
+        }else{
+            for(let i = l; i < r; i += 2){
+                const left = diffDfs(dp, l, i, ops);
+                const right = diffDfs(dp, i + 2, r, ops);
+                for(const lv of left){
+                    for(const rv of right){
+                        if(ops[i + 1] === ADDITION){
+                            dp[l][r].push(lv + rv);
+                        }else if(ops[i + 1] === SUBTRACTION){
+                            dp[l][r].push(lv - rv);
+                        }else{
+                            dp[l][r].push(lv * rv);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return dp[l][r];
+}
+
+const isDigit = (ch) => {
+    return parseFloat(ch).toString() === 'NaN' ? false : true;
+}
+
+// const expression = "2-1-1";
+const expression = "2*3-4*5";
+console.log('diffWaysToCompute:', diffWaysToCompute(expression));
 
 
 
