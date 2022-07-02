c557b19fc07d418a8ea52c314eddda83978f34f4
liukai <15818524733@139.com>
2022-07-02 21:56:11 +0800

    最低加油次数(leetcode 871)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2708,12 +2708,47 @@ const grayCode = (n) => {
 }
 
 // const n = 2;
-const n = 1;
-console.log('grayCode:', grayCode(n));
+// const n = 1;
+// console.log('grayCode:', grayCode(n));
 
 
+// 76.最低加油次数(leetcode 871)
+/**
+ * 汽车从起点出发驶向目的地，该目的地位于出发位置东面 target 英里处。
+ * 沿途有加油站，每个 station[i] 代表一个加油站，它位于出发位置东面 station[i][0] 英里处，并且有 station[i][1] 升汽油。
+ * 假设汽车油箱的容量是无限的，其中最初有 startFuel 升燃料。它每行驶 1 英里就会用掉 1 升汽油。
+ * 当汽车到达加油站时，它可能停下来加油，将所有汽油从加油站转移到汽车中。
+ * 为了到达目的地，汽车所必要的最低加油次数是多少？如果无法到达目的地，则返回 -1 。
+ * 注意：如果汽车到达加油站时剩余燃料为 0，它仍然可以在那里加油。如果汽车到达目的地时剩余燃料为 0，仍然认为它已经到达目的地。
+*/
+const minRefuelStops = (target, startFuel, stations) => {
+    const pq = [];
+    let ans = 0, prev = 0, fuel = startFuel;
+    const n = stations.length;
+    for(let i = 0; i <= n; i++){
+        const cur  = i < n ? stations[i][0] : target;
+        fuel -= cur - prev;
+        while(fuel < 0 && pq.length !== 0){
+            fuel += pq.shift();
+            ans++;
+        }
+        if(fuel < 0){
+            return -1;
+        }
+        if(i < n){
+            pq.push(stations[i][1]);
+            pq.sort((a, b) => b - a);
+            prev = cur;
+        }
+    }
+    return ans;
+}
 
-
+// const target = 1, startFuel = 1, stations = [];
+// const target = 100, startFuel = 1, stations = [[10,100]];
+const target = 100, startFuel = 10, stations = [[10,60],[20,30],[30,30],[60,40]];
+// const target = 100, startFuel = 50, stations = [[25,25],[50,50]];
+console.log('minRefuelStops:', minRefuelStops(target, startFuel, stations));
 
 
 
