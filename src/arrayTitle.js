ec95f305dc8d0167eaa4bec863c662b5b26a2e67
LuckyLiu123 <18229886430@163.com>
2022-07-11 10:23:42 +0800

    实现一个魔法字典(leetcode 676)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -2884,20 +2884,53 @@ const lenLongestFibSubseq = (arr) => {
     return ans;
 }
 
-const arr = [1,2,3,4,5,6,7,8];
+// const arr = [1,2,3,4,5,6,7,8];
 // const arr = [1,3,7,11,12,14,18];
-console.log('lenLongestFibSubseq:', lenLongestFibSubseq(arr));
+// console.log('lenLongestFibSubseq:', lenLongestFibSubseq(arr));
 
 
+// 79.实现一个魔法字典(leetcode 676)
+/**
+ * 设计一个使用单词列表进行初始化的数据结构，单词列表中的单词 互不相同 。 如果给出一个单词，
+ * 请判定能否只将这个单词中一个字母换成另一个字母，使得所形成的新单词存在于你构建的字典中。
+ * 实现 MagicDictionary 类：
+ * - MagicDictionary() 初始化对象
+ * - void buildDict(String[] dictionary) 使用字符串数组 dictionary 设定该数据结构，dictionary 中的字符串互不相同
+ * - bool search(String searchWord) 给定一个字符串 searchWord ，判定能否只将字符串中 一个 字母换成另一个字母，
+ *   使得所形成的新字符串能够与字典中的任一字符串匹配。如果可以，返回 true ；否则，返回 false 。
+*/
+const MagicDictionary = () => {
+    
+}
 
+MagicDictionary.prototype.buildDict = (dictionary) => {
+    this.words = dictionary;
+}
 
+MagicDictionary.prototype.search = (searchWord) => {
+    for(const word of this.words){
+        if(word.length !== searchWord.length){
+            continue;
+        }
+        let diff = 0;
+        for(let i = 0; i < word.length; i++){
+            if(word[i] !== searchWord[i]){
+                diff++;
+                if(diff > 1){
+                    break;
+                }
+            }
+        }
+        if(diff === 1){
+            return true;
+        }
+    }
+    return false;
+}
 
-
-
-
-
-
-
+var obj = new MagicDictionary()
+obj.buildDict(dictionary)
+var param_2 = obj.search(searchWord)
 
 
 
