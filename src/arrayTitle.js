71c1c79af3254a80bb1ded9c0dc20ee02941368c
LuckyLiu123 <18229886430@163.com>
2022-07-14 10:19:00 +0800

    前缀和后缀搜索(leetcode 745)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3027,13 +3027,38 @@ const asteroidCollision = (asteroids) => {
 // const asteroids = [5,10,-5];
 // const asteroids = [8,-8];
 // const asteroids = [10,2,-5];
-const asteroids = [-10,-2,5];
-console.log('asteroidCollision:', asteroidCollision(asteroids));
+// const asteroids = [-10,-2,5]; 
+// console.log('asteroidCollision:', asteroidCollision(asteroids));
 
 
+// 82. 前缀和后缀搜索(leetcode 745)
+/**
+ * 设计一个包含一些单词的特殊词典，并能够通过前缀和后缀来检索单词。
+ * 实现 WordFilter 类：
+ * - WordFilter(string[] words) 使用词典中的单词 words 初始化对象。
+ * f(string pref, string suff) 返回词典中具有前缀 prefix 和后缀 suff 的单词的下标。
+ * 如果存在不止一个满足要求的下标，返回其中 最大的下标 。如果不存在这样的单词，返回 -1 。
+*/
+// 方法一：计算每个单词的前缀后缀组合可能性
+const WordFilter = function(words) {
+    this.dictionary = new Map();
+    for(let i = 0; i < words.length; i++){
+        const word = words[i];
+        const m = word.length;
+        for(let prefixLen = 1; prefixLen <= m; prefixLen++){
+            for(let suffLen = 1; suffLen <= m; suffLen++){
+                this.dictionary.set(word.substring(0, prefixLen) + '#' + word.substring(m - suffLen) , i);
+            }
+        }
+    }
+}
 
-
-
+WordFilter.prototype.f = function(pref, suff){
+    if(this.dictionary.has(pref + '#' + suff)){
+        return this.dictionary.get(pref + '#' + suff);
+    }
+    return -1;
+}
 
 
 
