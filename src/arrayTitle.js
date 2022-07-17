a005b4db1bfcb0d7a1711693351ae21e398e3a67
liukai <15818524733@139.com>
2022-07-17 21:38:18 +0800

    数组嵌套(leetcode 565)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3108,8 +3108,29 @@ WordFilter.prototype.f = function(pref, suff){
 }
 
 
+// 83. 数组嵌套(leetcode 565)
+// 索引从0开始长度为N的数组A，包含0到N - 1的所有整数。找到最大的集合S并返回其大小，
+// 其中 S[i] = {A[i], A[A[i]], A[A[A[i]]], ... }且遵守以下的规则。
+// 假设选择索引为i的元素A[i]为S的第一个元素，S的下一个元素应该是A[A[i]]，之后是A[A[A[i]]]... 以此类推，
+// 不断添加直到S出现重复的元素。
+const arrayNesting = (nums) => {
+    const n = nums.length;
+    let ans = 0;
+    const vis = new Array(n).fill(0);
+    for(let i = 0; i < n; i++){
+        let cnt = 0;
+        while(!vis[i]){
+            vis[i] = true;
+            i = nums[i];
+            cnt++;
+        }
+        ans = Math.max(ans, cnt);
+    }
+    return ans;
+}
 
-
+const A = [5,4,0,3,1,6,2];
+console.log('arrayNesting:', arrayNesting(A));
 
 
 
