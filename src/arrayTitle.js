d5473e063c051f6e6c388a91162e2dc3cf33abbb
LuckyLiu123 <18229886430@163.com>
2022-07-19 10:16:35 +0800

    我的日程安排表 II(leetcode 731)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3226,16 +3226,52 @@ const getHash = (x, y) => {
 
 // const isInfected = [[0,1,0,0,0,0,0,1],[0,1,0,0,0,0,0,1],[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0]];
 // const isInfected = [[1,1,1],[1,0,1],[1,1,1]];
-const isInfected = [[1,1,1,0,0,0,0,0,0],[1,0,1,0,1,1,1,1,1],[1,1,1,0,0,0,0,0,0]];
-console.log('containVirus:', containVirus(isInfected));
+// const isInfected = [[1,1,1,0,0,0,0,0,0],[1,0,1,0,1,1,1,1,1],[1,1,1,0,0,0,0,0,0]];
+// console.log('containVirus:', containVirus(isInfected));
 
 
+// 85. 我的日程安排表 II(leetcode 731)
+/**
+ * 实现一个 MyCalendar 类来存放你的日程安排。如果要添加的时间内不会导致三重预订时，则可以存储这个新的日程安排。
+ * MyCalendar 有一个 book(int start, int end)方法。它意味着在 start 到 end 时间内增加一个日程安排，注意，
+ * 这里的时间是半开区间，即 [start, end), 实数 x 的范围为，  start <= x < end。
+ * 当三个日程安排有一些时间上的交叉时（例如三个日程安排都在同一时间内），就会产生三重预订。
+ * 每次调用 MyCalendar.book方法时，如果可以将日程安排成功添加到日历中而不会导致三重预订，返回 true。否则，返回 false 
+ * 并且不要将该日程安排添加到日历中。
+ * 请按照以下步骤调用MyCalendar 类: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
+*/
+const MyCalendarTwo = function() {
+    this.booked = [];
+    this.overlaps = [];
+}
 
+MyCalendarTwo.prototype.book = function(start, end) {
+    for(const arr of this.overlaps){
+        let l = arr[0], r = arr[1];
+        if(start < r && end > l){
+            return false;
+        }
+    }
 
+    for(const arr of this.booked){
+        const l = arr[0], r = arr[1];
+        if(start < r && end > l){
+            this.overlaps.push([Math.max(l, start), Math.min(r, end)]);
+        }
+    }
 
+    this.booked.push([start, end]);
+    return true;
+}
 
-
-
+const obj = new MyCalendarTwo()
+const param_1 = obj.book(10, 20)
+const param_2 = obj.book(50, 60)
+const param_3 = obj.book(10, 40)
+const param_4 = obj.book(5, 15)
+const param_5 = obj.book(5, 10)
+const param_6 = obj.book(25, 55)
+console.log('param:', param_1, param_2, param_3, param_4, param_5, param_6);
 
 
 
