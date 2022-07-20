92cb18bd34340ff7828c434722e6982801a58dc9
LuckyLiu123 <18229886430@163.com>
2022-07-20 15:02:20 +0800

    二维网格迁移(leetcode 1260)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3264,21 +3264,41 @@ MyCalendarTwo.prototype.book = function(start, end) {
     return true;
 }
 
-const obj = new MyCalendarTwo()
-const param_1 = obj.book(10, 20)
-const param_2 = obj.book(50, 60)
-const param_3 = obj.book(10, 40)
-const param_4 = obj.book(5, 15)
-const param_5 = obj.book(5, 10)
-const param_6 = obj.book(25, 55)
-console.log('param:', param_1, param_2, param_3, param_4, param_5, param_6);
+// const obj = new MyCalendarTwo()
+// const param_1 = obj.book(10, 20)
+// const param_2 = obj.book(50, 60)
+// const param_3 = obj.book(10, 40)
+// const param_4 = obj.book(5, 15)
+// const param_5 = obj.book(5, 10)
+// const param_6 = obj.book(25, 55)
+// console.log('param:', param_1, param_2, param_3, param_4, param_5, param_6);
 
 
+// 86. 二维网格迁移(leetcode 1260)
+/**
+ * 给你一个 m 行 n 列的二维网格 grid 和一个整数 k。你需要将 grid 迁移 k 次。
+ * 每次「迁移」操作将会引发下述活动：
+ * - 位于 grid[i][j] 的元素将会移动到 grid[i][j + 1]。
+ * - 位于 grid[i][n - 1] 的元素将会移动到 grid[i + 1][0]。
+ * - 位于 grid[m - 1][n - 1] 的元素将会移动到 grid[0][0]。
+ * 请你返回 k 次迁移操作后最终得到的 二维网格。
+*/
+const shiftGrid = (grid, k) => {
+    const m = grid.length, n = grid[0].length;
+    const ret = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            const index = (i * n + j + k) % (m * n);
+            ret[Math.floor(index / n)].splice(index % n, 1, grid[i][j]);
+        }
+    }
+    return ret;
+}
 
-
-
-
-
+// const grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1;
+// const grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4;
+const grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9;
+console.log('shiftGrid:', shiftGrid(grid, k));
 
 
 
