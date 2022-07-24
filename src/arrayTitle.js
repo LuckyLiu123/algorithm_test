8c2a396e32ebe2a9460d82e14defe6962031b1ff
liukai <15818524733@139.com>
2022-07-24 10:05:21 +0800

    公交站间的距离(leetcode 1184)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3437,14 +3437,39 @@ const sequenceReconstruction = (nums, sequences) => {
 
 // const nums = [1,2,3], sequences = [[1,2],[1,3]];
 // const nums = [1,2,3], sequences = [[1,2]];
-const nums = [1,2,3], sequences = [[1,2],[1,3],[2,3]];
-console.log('sequenceReconstruction:', sequenceReconstruction(nums, sequences));
+// const nums = [1,2,3], sequences = [[1,2],[1,3],[2,3]];
+// console.log('sequenceReconstruction:', sequenceReconstruction(nums, sequences));
 
 
+// 89. 公交站间的距离(leetcode 1184)
+/**
+ * 环形公交路线上有 n 个站，按次序从 0 到 n - 1 进行编号。我们已知每一对相邻公交站之间的距离，distance[i] 
+ * 表示编号为 i 的车站和编号为 (i + 1) % n 的车站之间的距离。
+ * 环线上的公交车都可以按顺时针和逆时针的方向行驶。
+ * 返回乘客从出发点 start 到目的地 destination 之间的最短距离。
+*/
+const distanceBetweenBusStops = (distance, start, destination) => {
+    if(start > destination){
+        const temp = start;
+        start = destination;
+        destination = temp;
+    }
+    let sum1 = 0, sum2 = 0;
+    for(let i = 0; i < distance.length; i++){
+        if(i >= start && i < destination){
+            sum1 += distance[i];
+        }else{
+            sum2 += distance[i];
+        }
+    }
+    return Math.min(sum1, sum2);
+}
 
-
-
-
+// const distance = [1,2,3,4], start = 0, destination = 1;
+// const distance = [1,2,3,4], start = 0, destination = 2;
+// const distance = [1,2,3,4], start = 0, destination = 3;
+const distance = [8,11,6,7,10,11,2], start = 0, destination = 3;
+console.log('distanceBetweenBusStops:', distanceBetweenBusStops(distance, start, destination));
 
 
 
