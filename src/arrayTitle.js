cecfaea817b3f352865149ced771261e19ca3315
LuckyLiu123 <18229886430@163.com>
2022-07-26 11:27:16 +0800

    滑动窗口的平均值(剑指 Offer II 041)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3571,7 +3571,28 @@ class SkiplistNode {
 }
 
 
+// 91. 滑动窗口的平均值(剑指 Offer II 041)
+/**
+ * 给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算滑动窗口里所有数字的平均值。
+ * 实现 MovingAverage 类：
+ * - MovingAverage(int size) 用窗口大小 size 初始化对象。
+ * - double next(int val) 成员函数 next 每次调用的时候都会往滑动窗口增加一个整数，
+ *   请计算并返回数据流中最后 size 个值的移动平均值，即滑动窗口里所有数字的平均值。
+*/
+const MovingAverage = function(size){
+  this.queue = [];
+  this.size = size;
+  this.sum = 0;
+}
 
+MovingAverage.prototype.next = function(val){
+    if(this.queue.length === this.size){
+      this.sum -= this.queue.shift();
+    }
+    this.queue.push(val);
+    this.sum += val;
+    return this.sum / this.queue.length;
+}
 
 
 
