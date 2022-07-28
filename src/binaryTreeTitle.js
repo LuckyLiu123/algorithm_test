56290f2b9d3cd65de0b5c237918d1c5c36f17ab7
LuckyLiu123 <18229886430@163.com>
2022-07-28 11:21:46 +0800

    完全二叉树插入器(leetcode 919)

--- src/binaryTreeTitle.js
+++ src/binaryTreeTitle.js
@@ -264,3 +264,126 @@ const pruneTree = (root) => {
   }
   return root;
 };
+
+
+// 9.完全二叉树插入器(leetcode 919)
+/**
+ * 完全二叉树 是每一层（除最后一层外）都是完全填充（即，节点数达到最大）的，并且所有的节点都尽可能地集中在左侧。
+ * 设计一种算法，将一个新节点插入到一个完整的二叉树中，并在插入后保持其完整。
+ * 实现 CBTInserter 类:
+ * - CBTInserter(TreeNode root) 使用头节点为 root 的给定树初始化该数据结构；
+ * - CBTInserter.insert(int v)  向树中插入一个值为 Node.val == val的新节点 TreeNode。使树保持完全二叉树的状态，并返回插入节点 TreeNode 的父节点的值；
+ * - CBTInserter.get_root() 将返回树的头节点。
+*/
+const CBTInserter = (root) => {
+  this.candidate = [];
+  this.root = root;
+
+  const queue = [];
+  queue.push(root);
+
+  while(queue.length){
+    const node = queue.shift();
+    if(node.left){
+      queue.push(node.left);
+    }
+    if(node.right){
+      queue.push(node.right);
+    }
+    if(!(node.left && node.right)){
+      this.candidate.push(node);
+    }
+  }
+}
+
+CBTInserter.prototype.insert = (val) => {
+  const child = new TreeNode(val);
+  const node = this.candidate[0];
+  const ret = node.val;
+  if(!node.left){
+    node.left = child;
+  }else{
+    node.right = child;
+    this.candidate.shift();
+  }
+  this.candidate.push(child);
+  return ret;
+}
+
+CBTInserter.prototype.get_root = () => {
+  return this.root;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
