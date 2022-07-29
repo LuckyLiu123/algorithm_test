759553531f75cd40b38dbce553caa54e1ca5cfd8
LuckyLiu123 <18229886430@163.com>
2022-07-29 11:01:12 +0800

    Lisp 语法解析(leetcode 736)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3625,21 +3625,67 @@ const arrayRankTransform = (arr) => {
 
 // const arr = [40,10,20,30];
 // const arr = [100,100,100];
-const arr = [37,12,28,9,100,56,80,5,12];
-console.log('arrayRankTransform:', arrayRankTransform(arr));
+// const arr = [37,12,28,9,100,56,80,5,12];
+// console.log('arrayRankTransform:', arrayRankTransform(arr));
 
 
+// 93. 有效的正方形(leetcode 593)
+/**
+ * 给定2D空间中四个点的坐标 p1, p2, p3 和 p4，如果这四个点构成一个正方形，则返回 true 。
+ * 点的坐标 pi 表示为 [xi, yi] 。输入 不是 按任何顺序给出的。
+ * 一个 有效的正方形 有四条等边和四个等角(90度角)。
+*/
+const validSquare = (p1, p2, p3, p4) => {
+  if(isEqual(p1, p2)){
+    return false;
+  }
+  if(help(p1, p2, p3, p4)){
+    return true;
+  }
+  if(isEqual(p1, p3)){
+    return false;
+  }
+  if(help(p1, p3, p2, p4)){
+    return true;
+  }
+  if(isEqual(p1, p4)){
+    return false;
+  }
+  if(help(p1, p4, p2, p3)){
+    return true;
+  }
+  return false;
+}
 
+const help = (p1, p2, p3, p4) => {
+  const v1 = [p1[0] - p2[0], p1[1] - p2[1]];
+  const v2 = [p3[0] - p4[0], p3[1] - p4[1]];
+  if(checkMidPoint(p1, p2, p3, p4) && checkLength(v1, v2) && calCos(v1, v2)){
+    return true;
+  }
+  return false;
+}
 
+const calCos = (v1, v2) => {
+  return (v1[0] * v2[0] + v1[1] * v2[1]) === 0;
+}
 
+const checkLength = (v1, v2) => {
+  return (v1[0] * v1[0] + v1[1] * v1[1]) === (v2[0] * v2[0] + v2[1] * v2[1]);
+}
 
+const checkMidPoint = (p1, p2, p3, p4) => {
+  return (p1[0] + p2[0]) === (p3[0] + p4[0]) && (p1[1] + p2[1]) === (p3[1] + p4[1]);
+}
 
+const isEqual = (a, b) => {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
 
-
-
-
-
-
+// const p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1];
+// const p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,12];
+// const p1 = [1,0], p2 = [-1,0], p3 = [0,1], p4 = [0,-1];
+// console.log('validSquare:', validSquare(p1, p2, p3, p4));
 
 
 
--- src/stringTitle.js
+++ src/stringTitle.js
@@ -287,8 +287,8 @@ const isDigit = (ch) => {
 };
 
 // const expression = "2-1-1";
-const expression = "2*3-4*5";
-console.log('diffWaysToCompute:', diffWaysToCompute(expression));
+// const expression = "2*3-4*5";
+// console.log('diffWaysToCompute:', diffWaysToCompute(expression));
 
 // 9. 单词替换(leetcode 648)
 /**
@@ -393,6 +393,123 @@ const gcb = (a, b) => {
 // console.log('fractionAddition:', fractionAddition(expression));
 
 
+// 11. Lisp 语法解析(leetcode 736)
+/**
+ * 给你一个类似 Lisp 语句的字符串表达式 expression，求出其计算结果。
+ * 表达式语法如下所示:
+ * - 表达式可以为整数，let 表达式，add 表达式，mult 表达式，或赋值的变量。表达式的结果总是一个整数。
+ * - (整数可以是正整数、负整数、0)
+ * - let 表达式采用 "(let v1 e1 v2 e2 ... vn en expr)" 的形式，其中 let 总是以字符串 "let"来表示，
+ *   接下来会跟随一对或多对交替的变量和表达式，也就是说，第一个变量 v1被分配为表达式 e1 的值，第二个变量 v2 被
+ *   分配为表达式 e2 的值，依次类推；最终 let 表达式的值为 expr表达式的值。
+ * - add 表达式表示为 "(add e1 e2)" ，其中 add 总是以字符串 "add" 来表示，该表达式总是包含两个表达式 e1、e2 ，
+ *   最终结果是 e1 表达式的值与 e2 表达式的值之 和 。
+ * - mult 表达式表示为 "(mult e1 e2)" ，其中 mult 总是以字符串 "mult" 表示，该表达式总是包含两个表达式 e1、e2，
+ *   最终结果是 e1 表达式的值与 e2 表达式的值之 积 。
+ * - 在该题目中，变量名以小写字符开始，之后跟随 0 个或多个小写字符或数字。为了方便，"add" ，"let" ，"mult" 会被定义
+ *   为 "关键字" ，不会用作变量名。
+ * - 最后，要说一下作用域的概念。计算变量名所对应的表达式时，在计算上下文中，首先检查最内层作用域（按括号计），
+ *   然后按顺序依次检查外部作用域。测试用例中每一个表达式都是合法的。有关作用域的更多详细信息，请参阅示例。
+*/
+const evaluate = (expression) => {
+  const scope = new Map();
+  let start = 0;
+
+  const innerEvaluate = (expression) => {
+    if(expression[start] !== '('){   // 非表达式，可能为：整数或变量
+      if(isLowerCase(expression[start])){
+        const v = parseVar(expression);  // 变量
+        const n = scope.get(v).length;
+        return scope.get(v)[n - 1];
+      }else{
+        return parseInt(expression);
+      }
+    }
+
+    let ret;
+    start++;
+    if(expression[start] === 'l'){
+      start += 4;
+      const vars = [];
+      while(true){
+        if(!isLowerCase(expression[start])){
+          ret = innerEvaluate(expression);  // let 表达式的最后一个 expr 表达式的值
+          break;
+        }
+        const v = parseVar(expression);
+        if(expression[start] === ')'){
+          const n = scope.get(v).length;
+          ret = scope.get(v)[n - 1];  // let 表达式的最后一个 expr 表达式的值
+          break;
+        }
+        vars.push(v);
+        start++;  // 移除空格
+        const e = innerEvaluate(expression);
+        if(!scope.has(v)){
+          scope.set(v, []);
+        }
+        scope.get(v).push(e);
+        start++;  // 移除空格
+      }
+      for(const v of vars){
+        scope.get(v).pop();  // 清除当前作用域的变量
+      }
+    }else if(expression[start] === 'a'){  // "add" 表达式
+      start += 4;  // 移除 "add "
+      const e1 = innerEvaluate(expression);
+      start++;  // 移除空格
+      const e2 = innerEvaluate(expression);
+      ret = e1 + e2;
+    }else{  // "mult" 表达式
+      start += 5;  // 移除 "mult "
+      const e1 = innerEvaluate(expression);
+      start++;  // 移除空格
+      const e2 = innerEvaluate(expression);
+      ret = e1 * e2;
+    }
+    start++;  // 移除右括号
+    return ret;
+  }
+
+  const parseInt = (expression) => {
+    const n = expression.length;
+    let ret = 0, sign = 1;
+    if(expression[start] === '-'){
+      sign = -1;
+      start++;
+    }
+    while(start < n && isDigit(expression[start])){
+      ret = ret * 10 + (expression.charAt(start) - '0');
+      start++;
+    }
+    return sign * ret;
+  }
+
+  const parseVar = (expression) => {
+    const n = expression.length;
+    let ret = '';
+    while(start < n && expression[start] !== ' ' && expression[start] !== ')'){
+      ret += expression[start];
+      start++;
+    }
+    return ret;
+  }
+  
+  return innerEvaluate(expression, start);
+}
+
+// const isDigit = (ch) => {
+//   return parseFloat(ch).toString() === 'NaN' ? false : true;
+// }
+
+const isLowerCase = (ch) => {
+  return ch >= 'a' && ch <= 'z';
+}
+
+// const expression = "(let x 2 (mult x (let x 3 y 4 (add x y))))";
+// const expression = "(let x 3 x 2 x)";
+const expression = "(let x 1 y 2 x (add x y) (add x y))";
+console.log('evaluate:', evaluate(expression));
 
 
 
