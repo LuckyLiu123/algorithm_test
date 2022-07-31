57e6835a2283cc83221bd55f52c363e41073f84a
liukai <15818524733@139.com>
2022-07-31 10:50:47 +0800

    最大层内元素和(leetcode 1161)

--- src/binaryTreeTitle.js
+++ src/binaryTreeTitle.js
@@ -315,9 +315,36 @@ CBTInserter.prototype.get_root = () => {
 }
 
 
-
-
-
+// 10. 最大层内元素和(leetcode 1161)
+/**
+ * 给你一个二叉树的根节点 root。设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
+ * 请返回层内元素之和 最大 的那几层（可能只有一层）的层号，并返回其中 最小 的那个。
+*/
+// 方法一：深度优先搜索
+const maxLevelSum = (root) => {
+  const sum = [];
+  const dfs = (node, level) => {
+    if(level === sum.length){
+      sum.push(node.val);
+    }else{
+      sum.splice(level, 1, sum[level] + node.val);
+    }
+    if(node.left){
+      dfs(node.left, level + 1);
+    }
+    if(node.right){
+      dfs(node1.right, level + 1);
+    }
+  }
+  dfs(root, 0);
+  let ans = 0;
+  for(let i = 0; i < sum.length; i++){
+    if(sum[i] > sum[ans]){
+      ans = i;
+    }
+  }
+  return ans + 1;
+}
 
 
 
