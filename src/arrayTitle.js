b07e65be625fc83ebfbc0daabe5bc06b8e970e0e
LuckyLiu123 <18229886430@163.com>
2022-08-04 09:44:46 +0800

    非递增顺序的最小子序列(leetcode 1403)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3767,7 +3767,7 @@ class UnionFind {
  * - isEmpty(): 检查循环队列是否为空。
  * - isFull(): 检查循环队列是否已满。
 */
-const MyCircularQueue = (k) => {
+const MyCircularQueue = function(k) {
   this.capacity = k + 1;
   this.elements = new Array(this.capacity).fill(0);
   this.rear = 0;
@@ -3813,9 +3813,46 @@ MyCircularQueue.prototype.isFull = () => {
 }
 
 
+// 96. 非递增顺序的最小子序列(leetcode 1403)
+/**
+ * 给你一个数组 nums，请你从中抽取一个子序列，满足该子序列的元素之和 严格 大于未包含在该子序列中的各元素之和。
+ * 如果存在多个解决方案，只需返回 长度最小 的子序列。如果仍然有多个解决方案，则返回 元素之和最大 的子序列。
+ * 与子数组不同的地方在于，「数组的子序列」不强调元素在原数组中的连续性，也就是说，它可以通过从数组中分离一些（也可能不分离）元素得到。
+ * 注意，题目数据保证满足所有约束条件的解决方案是 唯一 的。同时，返回的答案应当按 非递增顺序 排列。
+*/
+const minSubsequence = (nums) => {
+  // let sum = nums.reduce((total, cur) => total + cur, 0);
+  // const arr = nums.slice(0, nums.length);
+  // arr.sort((a, b) => b - a);
+  // const ans = [];
+  // for(let i = 0; i < arr.length; i++){
+  //   const sum1 = ans.reduce((total, cur) => total + cur, 0);
+  //   if(sum1 + arr[i] > sum - arr[i]){
+  //     ans.push(arr[i]);
+  //     return ans;
+  //   }else{
+  //     ans.push(arr[i]);
+  //     sum = sum - arr[i];
+  //   }
+  // }
+  let sum = nums.reduce((total, cur) => total + cur, 0);
+  nums.sort((a, b) => b - a);
+  const ans = [];
+  let curr = 0;
+  for(let i = 0; i < nums.length; i++){
+    curr += nums[i];
+    ans.push(nums[i]);
+    if(curr > sum - curr){
+      break;
+    }
+  }
+  return ans;
+}
 
-
-
+const nums = [4,3,10,9,8];
+// const nums = [4,4,7,6,7];
+// const nums = [6];
+console.log('minSubsequence:', minSubsequence(nums));
 
 
 
