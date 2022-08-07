4e2cfeb5866de7f5ac857b1bf6f836ec5ec67649
liukai <15818524733@139.com>
2022-08-07 11:24:54 +0800

    数组中的字符串匹配(leetcode 1408)

--- src/stringTitle.js
+++ src/stringTitle.js
@@ -526,8 +526,29 @@ const generateTheString = (n) => {
 }
 
 
+// 13. 数组中的字符串匹配(leetcode 1408)
+/**
+ * 给你一个字符串数组 words ，数组中的每个字符串都可以看作是一个单词。请你按 任意 顺序返回 words 
+ * 中是其他单词的子字符串的所有单词。
+ * 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 word[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
+*/
+const stringMatching = (words) => {
+  const res = [];
+  for(let i = 0; i < words.length; i++) {
+    for(let j = 0; j < words.length; j++) {
+      if(i !== j && words[j].indexOf(words[i]) > -1){
+        res.push(words[i]);
+        break;
+      }
+    }
+  }
+  return res;
+}
 
-
+// const words = ["mass","as","hero","superhero"];
+// const words = ["leetcode","et","code"];
+const words = ["blue","green","bu"];
+console.log('stringMatching:', stringMatching(words));
 
 
 
