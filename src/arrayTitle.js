bfa224f0ff2530b4ef3f44640ccf5ef765aa577a
LuckyLiu123 <18229886430@163.com>
2022-08-12 14:24:14 +0800

    用户分组(leetcode 1282)

--- src/arrayTitle.js
+++ src/arrayTitle.js
@@ -3897,14 +3897,48 @@ const exclusiveTime = (n, logs) => {
 // const n = 1, logs = ["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"];
 // const n = 2, logs = ["0:start:0","0:start:2","0:end:5","1:start:6","1:end:6","0:end:7"];
 // const n = 2, logs = ["0:start:0","0:start:2","0:end:5","1:start:7","1:end:7","0:end:8"];
-const n = 1, logs = ["0:start:0","0:end:0"];
-console.log('exclusiveTime:', exclusiveTime(n, logs));
+// const n = 1, logs = ["0:start:0","0:end:0"];
+// console.log('exclusiveTime:', exclusiveTime(n, logs));
 
 
+// 98. 用户分组(leetcode 1282)
+/**
+ * 有 n 个人被分成数量未知的组。每个人都被标记为一个从 0 到 n - 1 的唯一ID 。
+ * 给定一个整数数组 groupSizes ，其中 groupSizes[i] 是第 i 个人所在的组的大小。例如，如果 groupSizes[1] = 3 ，
+ * 则第 1 个人必须位于大小为 3 的组中。
+ * 返回一个组列表，使每个人 i 都在一个大小为 groupSizes[i] 的组中。
+ * 每个人应该 恰好只 出现在 一个组 中，并且每个人必须在一个组中。如果有多个答案，
+ * 返回其中 任何 一个。可以 保证 给定输入 至少有一个 有效的解。
+*/
+const groupThePeople = (groupSizes) => {
+  const groups = new Map();
+  const n = groupSizes.length;
+  for(let i = 0; i < n; i++){
+    const size = groupSizes[i];
+    if(!groups.has(size)){
+      groups.set(size, []);
+    }
+    groups.get(size).push(i);
+  }
 
+  const groupList = [];
+  for(const [size, people] of groups.entries()){
+    const groupCount = Math.floor(people.length / size);
+    for(let i = 0; i < groupCount; i++){
+      const group = [];
+      const start = i * size;
+      for(let j = 0; j < size; j++){
+        group.push(people[start + j]);
+      }
+      groupList.push(group);
+    }
+  }
+  return groupList;
+}
 
-
-
+// const groupSizes = [3,3,3,3,3,1,3];
+const groupSizes = [2,1,3,3,3,2];
+console.log('groupThePeople:', groupThePeople(groupSizes));
 
 
 
